40ed7e8068dcb98d3f3989c101105958fea18491
Alex1993St <36221545+Alex1993St@users.noreply.github.com>
2019-05-29 15:31:59 +0300

    parant child

---
+++ Test.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <test v-on:event_child="eventChild"></test>
+        {{get_child_info}}
+        <test :value="put_to_child"></test>
+    </div>
+</template>
+
+<script>
+    import Test2 from './Test2'
+
+    export default {
+        name: "Test",
+        data(){
+            return {
+                get_child_info:'',
+                put_to_child: 'Hello world'
+            }
+        },
+
+        components:{
+            'test': Test2
+        },
+
+        methods: {
+            eventChild: function(info) {
+                this.get_child_info = info
+            },
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Test2.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <!--<button v-on:click="emit">-->
+            <!--Clique me to emit from child component-->
+        <!--</button>-->
+        <div>{{value}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Test2",
+        props: ['value'],
+        data(){
+            return {
+                info: {
+                    title: 'test',
+                    name: '123'
+                },
+            }
+        },
+
+        created(){
+            this.$emit('event_child', this.info)
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
